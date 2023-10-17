5eea16bd31249484c275ed600facf8714c098c60
samgowtham <148205473+samgowtham@users.noreply.github.com>
2023-10-17 15:54:51 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,26 +1,30 @@
-# 2.2 implement a class called player that represents a cricket player. the player class should have a method called play() which prints " the player is playing cricket.derive two classes ,batsman and bowler,from the player class .override the play() Method in each derived class to print"the batsman is batting"and"the bowler is bowling",respectively.write a program to create objects of both the batsman and bowler classes and call the play()method for each object.
+# 3.2 implement a function called sort students that takes a list of student objects as input and sorts the list based on their CGPA(Cumulative Grade Point Average) in descending order.each student object has the following attributes: name(string),roll number(string), and cgpa(float).test the function with different input lists of student
 
-#define the base class player
-class Player:
-    def play(self):
-        print("the player is playing cricket.")
-      
-#define the derived class batsman
-class Batsman(Player):
-  def play(self):
-      print("The batsman is batting")
+class Student:
 
+  def __init__(self, name, roll_number, cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
 
-#define the derived class bowler
-class Bowler(Player):
-  def play(self):
-    print("The bowler is bowling")
-    
+def sort_students(student_lists):
+  #Sort the list of  students in descending order of CGPA
+  sorted_students = sorted(student_lists,
+                          key=lambda student: student.cgpa,
+                          reverse=True)
+  # Syntax - lambda arg:exp
+  return sorted_students
 
-#create objects of batsman and bowler classes
-batsman = Batsman()
-bowler = Bowler()
+#Example usage:
+students = [
+  Student("karthikeyan", "A40", 9.9),
+  Student("suriya", "A41", 7.5),
+  Student("vaithis", "A42", 8.5),
+  Student("veejay", "A43", 9.0),
+]
 
-#call the play() method for each object
-batsman.play()
-bowler.play()
+sorted_students = sort_students(students)
+
+# print the sorted list of students
+for student in sorted_students:
+  print("Name: {}, Roll Number: {}, CGPA: {}".format(student.name, student.roll_number, student.cgpa))
--- main.py
+++ main.py
@@ -1,40 +1,17 @@
-# 2.1 Implement a class called bankaccount that represents a bankaccount.the class should have private attributes for account number,account holder name and bank balance.include methods to deposit money,withdraw money and display the account balance.ensure that  the account balance cannot be accessed directly outside the class.write a program to create an instance of the bank account class and test the deposit and withdrawal functionality
+# 3.1 write a function called linear search product that takes the list of product and a target product  name  as input. the function should perform a linear search to find the target product in the list of indicesof all occurrences of the product if found,or an empty list if the product is not found
 
+def linearSearchProduct(productList, targetProduct):
+  indices = []
+  
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
 
-class bankaccount:
+  return indices
 
-    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
-        self.__account_number = account_number
-        self.__account_holder_name = account_holder_name
-        self.__account_balance = initial_balance
-
-    def deposit(self, amount):
-        if amount > 0:
-            self.__account_balance += amount
-            print("Deposited ₹{}. New balance: ₹{}".format(amount, self.__account_balance))
-        else:  
-            print("Invalid deposit amount.")
-
-    def withdraw(self, amount):
-        if amount > 0 and amount <= self.__account_balance:
-            self.__account_balance -= amount
-            print("Withdraw ₹{}. New balance: ₹{}".format(amount, self.__account_balance))
-        else:
-            print("Invalid withdrawal amount or insufficient balance.")
-
-    def display_balance(self):
-        print("Account balance for {} (Account #{}): ₹{}".format(
-            self.__account_holder_name, self.__account_number, self.__account_balance))
-
-
-# create an instance of the bankaccount class
-account = bankaccount(account_number="1234567890",
-                      account_holder_name="karthikeyan",
-                      initial_balance=100000.0)
-
-# test deposit and withdrawal functionality
-account.display_balance()
-account.deposit(500.0)
-account.withdraw(200.0)
-account.withdraw(200000.0)
-account.display_balance()
+# Example usage
+products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+target2 = 'apple'
+result = linearSearchProduct(products, target)
+print(result)
